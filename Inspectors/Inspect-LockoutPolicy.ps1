e061a19dba1af9041656d090ccde08faec910127
ThoughtContagion <clittrell@soteria.io>
2021-10-13 15:25:30 -0400

    New functionality

---
+++ Inspectors/Inspect-LockoutPolicy.ps1
@@ -0,0 +1,23 @@
+<#
+.SYNOPSIS
+    Gather information about the Active Directory Domain Account Lockout Policy
+.DESCRIPTION
+    This script checks Active Directory Active Directory Domain Account Lockout Policy
+.COMPONENT
+    PowerShell, Active Directory PowerShell Module
+.ROLE
+    Domain Admin or Delegated rights
+.FUNCTIONALITY
+    Gather information about Active Directory Active Directory Domain Account Lockout Policy
+#>
+
+
+Function Inspect-LockoutPolicy{
+    $domain = Get-ADDomain 
+    $AccountPolicy = Get-ADObject $domain.DNSRoot -Property lockoutDuration, lockoutObservationWindow, lockoutThreshold
+    $Info = $AccountPolicy | Select-Object @{n="lockoutDuration";e={"$($_.lockoutDuration / -600000000) minutes"}},@{n="lockoutObservationWindow";e={"$($_.lockoutObservationWindow / -600000000) minutes"}},lockoutThreshold | Format-List
+
+    Return $Info
+}
+
+Return Inspect-LockoutPolicy
