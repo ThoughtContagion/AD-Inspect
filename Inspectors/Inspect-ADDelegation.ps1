ed74812fd448d084623bfae21c700bb83015e0e7
ThoughtContagion <clittrell@soteria.io>
2021-11-18 12:45:21 -0500

    Bug Fixes - LAPS Readers

--- Inspectors/Inspect-ADDelegation.ps1
+++ Inspectors/Inspect-ADDelegation.ps1
@@ -1,16 +1,3 @@
-<#
-.SYNOPSIS
-    Gather information about Active Directory High-value target accounts
-.DESCRIPTION
-    This script checks Active Directory Accounts that are High-value targets and offers remediation steps
-.COMPONENT
-    PowerShell, Active Directory PowerShell Module, and sufficient rights to change admin accounts
-.ROLE
-    Domain Admin or Delegated rights
-.FUNCTIONALITY
-    Gather information about Active Directory high-value target accounts
-#>
-
 $path = @($out_path)
 Function Inspect-Delegation{
     $OUs = Get-ADOrganizationalUnit -Filter {Name -like '*'} 
